33115d9924f9bc5c037161e0189d3e3abcffa6fc
Łukasz Janus <lukasz.janus212@gmail.com>
2022-11-13 20:55:37 +0100

    Add script for titleClickHandler function usability

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 'use strict';
 
-// document.getElementById('test-button').addEventListener('click', function(){
-//     const links = document.querySelectorAll('.titles a');
-//     console.log('links:', links);
-//   });
-
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
@@ -21,8 +17,8 @@ const titleClickHandler = function(event){
     /* [DONE] add class 'active' to the clicked link */
 
     console.log('clickedElement:', clickedElement);
-    clickedElement.classList.add('active');
 
+    clickedElement.classList.add('active');
 
     /* [DONE] remove class 'active' from all articles */
   
@@ -32,11 +28,20 @@ const titleClickHandler = function(event){
         activeArticle.classList.remove('active');
     }
   
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+
+    const clickedLink = clickedElement.getAttribute('href');
+    console.log(clickedLink);
   
     /* find the correct article using the selector (value of 'href' attribute) */
+
+    const selectedArticle = document.querySelector(clickedLink);
+    console.log(selectedArticle);
   
-    /* add class 'active' to the correct article */
+    /* [DONE] add class 'active' to the correct article */
+
+    selectedArticle.classList.add('active');
+    console.log(selectedArticle);
   }
   
   const links = document.querySelectorAll('.titles a');
